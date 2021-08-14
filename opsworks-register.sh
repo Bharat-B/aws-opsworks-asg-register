d09e944f2f44aacc91439534f734b60627804ed2
Bharat B <bbharat95@gmail.com>
2021-08-14 23:41:18 +0530

    Update opsworks-register.sh
    Added fixes for dynamic hostname, which now accepts a hostname prefix from UserData and also suffixes them with a timestamp down to microseconds.

--- opsworks-register.sh
+++ opsworks-register.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (C) 2021 Bharat Bala <bbharat95@gmail.com>
+# Copyright (C) 2021 Bharat Bala <bharat.bala@opskins.com>
 # This file is free software; as a special exception the author gives
 # unlimited permission to copy and/or distribute it, with or without
 # modifications, as long as this notice is preserved.
@@ -9,33 +9,30 @@ USERDATA=$(curl -sL http://169.254.169.254/latest/user-data)
 INSTANCE_ID=$(curl -sL http://169.254.169.254/latest/meta-data/instance-id)
 REGION=`curl -sL http://169.254.169.254/latest/meta-data/placement/availability-zone | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'`
 
-#Parse the data to retrieve OpsWorks data 
+#Parse the data to retrieve OpsWorks data
+INSTANCE_PREFIX=`echo $USERDATA | jq -r .INSTANCE_PREFIX`
+TIMESTAMP=$(($(date +"%s%N")/1000000))
+OPSWORKS_HOSTNAME="$INSTANCE_PREFIX-$TIMESTAMP"
 OPSWORKS_STACK_ID=`echo $USERDATA | jq -r .STACK_ID`
 OPSWORKS_LAYER_ID=`echo $USERDATA | jq -r .LAYER_ID`
 
 #Default parameters for OpsWorks registration
-REGISTER_OPTS="--region $REGION --stack-id $OPSWORKS_STACK_ID --infrastructure-class ec2 --use-instance-profile --local"
+REGISTER_OPTS="--region $REGION --stack-id $OPSWORKS_STACK_ID --infrastructure-class ec2 --use-instance-profile --override-hostname $OPSWORKS_HOSTNAME --local"
 
 #Start Process
 
 #Check if the instance is already registered in the stack
 if [ ! -f "/var/lib/aws/opsworks/setup.done" ]; then
-
-        #Get Opsworks Compatible Instance Name
-        OPSWORKS_HOSTNAME=`aws opsworks get-hostname-suggestion --region $REGION --layer-id=$OPSWORKS_LAYER_ID --output=json | jq -r '.Hostname'`
-
+	#Create a Hostname
         #Assign the same to the EC2 Instance
         aws ec2 create-tags --region $REGION --resources $INSTANCE_ID --tags Key=Name,Value="$OPSWORKS_HOSTNAME"
-
         #Register the EC2 instance to OpsWorks
-        OPSWORKS_INSTANCE_ID=(aws opsworks register $REGISTER_OPTS --override-hostname $OPSWORKS_HOSTNAME | grep -o "Instance ID: .*" | cut -d':' -f2 | tr -d ' ')
-
+        OPSWORKS_INSTANCE_ID=$(aws opsworks register $REGISTER_OPTS | grep -o "Instance ID: .*" | cut -d':' -f2 | tr -d ' ')
         #Wait while the registration happens
         while [ ! -f "/var/lib/aws/opsworks/setup.done" ]
-        do
-                sleep 5
-        done
-
+	do
+		sleep 5
+	done
 fi
 
 if [ -f "/etc/motd" ] && [ -z "$OPSWORKS_INSTANCE_ID" ]; then
